16cb8535a1efd3a0c9617bfe7b5b83e46a998a16
TrafficLight42 <finleyturnerhall@protonmail.com>
2026-05-12 10:17:50 +0100

    Remove all non-Finley's work files. Add BLEanalysis to requirements.txt

--- BleFinley/setup.py
+++ BleFinley/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="BleFinley",
-    version="0.1",
+    version="1.0",
     packages=["."],
     author="Finley Turner-Hall",
     author_email="fturner-hall1@sheffield.ac.uk"
